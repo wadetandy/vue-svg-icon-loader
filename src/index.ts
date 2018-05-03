816d5c2c05973f3a8785bf0f9a57a058365526e3
Wade Tandy <wtandy@bloomberg.net>
2018-05-03 17:44:32 -0400

    Remove svg-sprite-loader dependency
    There's not a ton of benefit to the sprite loader in practice, so we're
    going to remove it in favor of a raw parsing of the SVG using
    vue-template-compiler.  We'll then pass the output along to the
    vue-svg-component-builder library.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,77 @@
 import * as webpack from 'webpack'
 import { getOptions, stringifyRequest } from 'loader-utils'
 import { resolve } from 'path'
+import { build } from 'vue-svg-component-builder'
+import { compile, ASTElement, ASTNode } from 'vue-template-compiler'
 
 interface IOptions {
   defaultScale?: number
 }
 
-const loader : webpack.loader.Loader = function(contents : string | Buffer) {
-  const options : IOptions = getOptions(this)
+const isElementNode = (node : ASTNode) : node is ASTElement => {
+  return node.hasOwnProperty('children')
 }
 
-loader.pitch = function(this: webpack.loader.LoaderContext, remainingRequest : string) {
-  const options : IOptions = getOptions(this) || { defaultScale: 1 }
-  const { defaultScale } = options
-  const requireString = stringifyRequest(this, "!!" + remainingRequest)
-  const builderPath = resolve(__dirname, './icon-builder')
+type MinifiedASTNode = { text: string } | MinifedASTElement
+
+interface MinifedASTElement {
+  tag: string,
+  attrsMap: Record<string, string>
+  children: MinifiedASTNode[]
+}
+
+/*
+ * We don't really use much of the final AST output in building our icons, so let's
+ * avoid the need to ouput the whole thing to the final bundle.
+ */
+function minifyAst(node: ASTNode) : MinifiedASTNode {
+  if (!isElementNode(node)) {
+    return {
+      text: node.text
+    }
+  }
+
+  delete node.parent
+
+  let children = node.children.map(element => {
+    return minifyAst(element)
+  })
+
+  let output = {
+    tag: node.tag,
+    attrsMap: node.attrsMap,
+    children
+  }
+
+  return output
+}
+
+const loader : webpack.loader.Loader = function(contents : string | Buffer) {
+  const options : IOptions = getOptions(this)
+
+  if(typeof contents !== 'string') {
+    contents = contents.toString()
+  }
+
+  const compiledSvg = compile(contents)
+  if (compiledSvg.ast === undefined) {
+    let errorMsg = 'Unknown Error'
+
+    if(compiledSvg.errors.length > 0) {
+      errorMsg = compiledSvg.errors.join(', ')
+    }
+
+    throw new Error(`There were one or more problems building the AST for the requested SVG: ${errorMsg}`)
+  }
+
+  const ast = minifyAst(compiledSvg.ast)
 
   return `
-    const svg = require(${requireString}).default
-    const buildIconComponent = require('${builderPath}').buildIconComponent
-
-    export default buildIconComponent(svg, ${defaultScale + ''})
+    var builder = require('vue-svg-component-builder')
+    module.exports = {
+      __esModule: true,
+      default: builder.build(${JSON.stringify(ast, null, 2)})
+    }
   `
 }
 
